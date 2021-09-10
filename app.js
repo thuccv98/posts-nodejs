37b5abf43ffaf8b7b2941842ac045a5c663b82de
thuccv98 <thuccv98@gmail.com>
2021-09-10 16:43:27 +0700

    Connect mongodb cloud atlas database

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
 
 // Khoi dong app
 const app = express();
+dotenv.config();
 
 // Khoi dong express middleware
 app.use(express.json());
+
+// Ket noi co so du lieu
+connectDB();
 
 const PORT = 5000;
 
